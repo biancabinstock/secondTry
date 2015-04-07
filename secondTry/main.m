e321a3f03302156159f096681e574f02ee307bdd
Bianca Binstock <bianca.pr@gmail.com>
2015-04-07 14:14:26 -0400

    Initial Commit

---
+++ secondTry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  secondTry
+//
+//  Created by Bianca Binstock on 2015-04-07.
+//  Copyright (c) 2015 Bianca Binstock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
